364b9917afcc267a3072b177377599d79f99b68e
arfanxn <arfan2173@gmail.com>
2022-12-09 16:49:01 +0700

    create custom button

---
+++ Card Workout/Components/CWButton.swift
@@ -0,0 +1,20 @@
+//
+//  CWButton.swift
+//  Card Workout
+//
+//  Created by Muhammad Arfan on 09/12/22.
+//
+
+import UIKit
+
+class CWButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
