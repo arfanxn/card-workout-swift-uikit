51440e0fd5bea6221d889f8af81dd22f3504a0eb
arfanxn <arfan2173@gmail.com>
2022-12-09 16:49:46 +0700

    update custom butn

--- Card Workout/Components/CWButton.swift
+++ Card Workout/Components/CWButton.swift
@@ -8,13 +8,21 @@
 import UIKit
 
 class CWButton: UIButton {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure () {
+        layer.cornerRadius = 10
+        titleLabel?.font = .systemFont(ofSize: 18 , weight: .bold)
+        setTitleColor(.white, for: .normal)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 
 }
