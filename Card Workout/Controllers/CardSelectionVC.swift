d266a1007b42245377dcac574c8cd3148c04fdb7
arfanxn <arfan2173@gmail.com>
2022-12-10 07:24:14 +0700

    make card selction controller and configure the ui

--- Card Workout/Controllers/CardSelectionVC.swift
+++ Card Workout/Controllers/CardSelectionVC.swift
@@ -18,12 +18,14 @@ class CardSelectionVC: UIViewController {
     }()
     let restartBtn : UIButton = {
         let btn = CWButton()
+        btn.configure()
         btn.setTitle("Restart", for: .normal)
         btn.backgroundColor = .systemGreen
         return btn
     }()
     let ruleInfoBtn : UIButton = {
         let btn = CWButton()
+        btn.configure()
         btn.setTitle("Rules", for: .normal)
         btn.backgroundColor = .systemBlue
         return btn
@@ -39,18 +41,54 @@ class CardSelectionVC: UIViewController {
     
     func configureUI () {
         self.configureCardImgView()
+        self.configureStopBtn()
+        self.configureRestartBtn()
+        self.configureRuleInfoBtn()
     }
     
     func configureCardImgView () {
         self.view.addSubview(self.cardImgVIew)
         self.cardImgVIew.translatesAutoresizingMaskIntoConstraints = false
-        self.cardImgVIew.image = .init(systemName: "heart.fill")
+        self.cardImgVIew.image = CardDeck.images.randomElement()
         
         NSLayoutConstraint.activate([
             self.cardImgVIew.widthAnchor.constraint(equalToConstant: 250),
             self.cardImgVIew.heightAnchor.constraint(equalToConstant: 350),
             self.cardImgVIew.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             self.cardImgVIew.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -75)
+        ])
+    }
+    
+    func configureStopBtn () {
+        self.view.addSubview(self.stopBtn)
+        
+        NSLayoutConstraint.activate([ 
+            self.stopBtn.widthAnchor.constraint(equalToConstant: 250),
+            self.stopBtn.heightAnchor.constraint(equalToConstant: 50),
+            self.stopBtn.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.stopBtn.topAnchor.constraint(equalTo: self.cardImgVIew.bottomAnchor, constant: 20)
+        ])
+    }
+    
+    func configureRestartBtn () {
+        self.view.addSubview(self.restartBtn)
+
+        NSLayoutConstraint.activate([
+            self.restartBtn.widthAnchor.constraint(equalToConstant: 115),
+            self.restartBtn.heightAnchor.constraint(equalToConstant: 50),
+            self.restartBtn.leadingAnchor.constraint(equalTo: self.stopBtn.leadingAnchor),
+            self.restartBtn.topAnchor.constraint(equalTo: self.stopBtn.bottomAnchor, constant: 20)
+        ])
+    }
+    
+    func configureRuleInfoBtn () {
+        self.view.addSubview(self.ruleInfoBtn)
+
+        NSLayoutConstraint.activate([
+            self.ruleInfoBtn.widthAnchor.constraint(equalToConstant: 115),
+            self.ruleInfoBtn.heightAnchor.constraint(equalToConstant: 50),
+            self.ruleInfoBtn.trailingAnchor.constraint(equalTo: self.stopBtn.trailingAnchor),
+            self.ruleInfoBtn.topAnchor.constraint(equalTo: self.stopBtn.bottomAnchor, constant: 20)
         ])
     }
     
